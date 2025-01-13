be3f053cb3fd48a0298211c0c2a8eb61710bdf85
adeka stanley <adekastanley2@gmail.com>
2025-01-13 20:46:02 +0100

    changed navigation style mobile

--- app/components/navigation/NavBar.tsx
+++ app/components/navigation/NavBar.tsx
@@ -2,6 +2,18 @@ import siteIcons from "~/assets/icons/siteIcons";
 
 import { Button } from "../ui/button";
 import { Link } from "react-router";
+import { FaBars } from "react-icons/fa";
+
+import {
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerDescription,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from "~/components/ui/drawer";
 
 const NavBar = () => {
 	const isTablet = typeof window !== "undefined" && window.innerWidth < 1200;
@@ -17,10 +29,34 @@ const MobileNav = () => {
 					<img className="w-20" src={siteIcons.logo} alt="qupid logo" />
 				</Link>
 			</div>
-			<div className="flex gap-10">
+			{/* <div className="flex gap-10">
 				<Button variant="outline" className="rounded-xl">
 					Download Qupid
 				</Button>
+			</div> */}
+			<div>
+				<Drawer>
+					<DrawerTrigger>
+						<FaBars size={32} />
+					</DrawerTrigger>
+					<DrawerContent>
+						{/* <DrawerHeader>
+							<DrawerTitle>Are you absolutely sure?</DrawerTitle>
+							<DrawerDescription>
+								This action cannot be undone.
+							</DrawerDescription>
+						</DrawerHeader> */}
+						<DrawerFooter>
+							<Button variant={"outline"} className="">
+								Sign Up
+							</Button>
+							<Button>Download On Android</Button>
+							{/* <DrawerClose>
+								<Button variant="outline">Cancel</Button>
+							</DrawerClose> */}
+						</DrawerFooter>
+					</DrawerContent>
+				</Drawer>
 			</div>
 		</header>
 	);
