a9c281b8a61b79890fa7309fce3826f4884e8cbb
adeka stanley <adekastanley2@gmail.com>
2025-01-13 21:42:59 +0100

    fix 7

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,12 @@
-// import type { Route } from "./+types/home";
+import type { Route } from "./+types/home";
 import HomePage from "~/components/Pages/Home/Home";
 
-// export function meta({}: Route.MetaArgs) {
-// 	return [
-// 		{ title: "Qupid app" },
-// 		{ name: "description", content: "Qupid app description" },
-// 	];
-// }
+export function meta({}: Route.MetaArgs) {
+	return [
+		{ title: "Qupid app" },
+		{ name: "description", content: "Qupid app description" },
+	];
+}
 
 export default function Home() {
 	return (
