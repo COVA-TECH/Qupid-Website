28e54f2551495a366bd28de12ff885794926cb3b
adeka stanley <adekastanley2@gmail.com>
2025-01-17 15:26:51 +0100

    upload

--- text.tsx
+++ text.tsx
@@ -1,3 +1,10 @@
 // "build": "cross-env NODE_ENV=production react-router build",
 
 // "build": "tsc -b && vite build",
+
+// "builds": [
+//     {
+//         "src": "dist",
+//         "use": "@vercel/static"
+//     }
+// ]
